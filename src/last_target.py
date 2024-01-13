6d1ac9927c3f918b44f2032472dc3ffe8671c37e
Mehdi <m.raza@rewaatech.com>
2024-01-13 16:05:23 +0000

    avg_velocity, dist_to_goal, target_pose services

---
+++ src/last_target.py
@@ -0,0 +1,22 @@
+#! /usr/bin/env python
+
+import rospy
+from monitoring_package.srv import pose, poseResponse
+lastTarget = None
+
+def getLastTarget(request):
+    global lastTarget
+    xGoal = rospy.get_param('des_pos_x')
+    yGoal = rospy.get_param('des_pos_y')
+    return poseResponse(xGoal, yGoal)
+
+
+
+def main():
+    rospy.init_node('get_last_target_service', log_level=rospy.DEBUG)
+    s = rospy.Service('get_last_target', pose, getLastTarget)
+    rospy.spin()
+
+if __name__ == "__main__":
+    main()
+
--- src/planning_client.py
+++ src/planning_client.py
@@ -6,8 +6,15 @@ import rospy
 import actionlib
 import assignment_2_2023.msg
 from geometry_msgs.msg import PoseStamped
+from nav_msgs.msg import Odometry
+from monitoring_package.msg import poseVelocity
 
+poseVelocityPublisher = None
+poseVelocitySubscriber = None
 def planning_client():
+    """
+	This function is responsible to send goal pose to the action server. 
+    """
     client = actionlib.SimpleActionClient('/reaching_goal', assignment_2_2023.msg.PlanningAction)
     print("planning_client waiting for server")
     client.wait_for_server()
@@ -19,15 +26,27 @@ def planning_client():
     goal = assignment_2_2023.msg.PlanningGoal(target_pose=pose)
 
     client.send_goal(goal)
-    client.wait_for_result()
-    return client.get_result()
+
+def publishPoseAndVelocity(msg):
+    poseVelocityMsg = poseVelocity()
+    poseVelocityMsg.x = msg.pose.pose.position.x
+    poseVelocityMsg.y = msg.pose.pose.position.y
+    poseVelocityMsg.vel_x = msg.twist.twist.linear.x
+    poseVelocityMsg.vel_y = msg.twist.twist.linear.y
+    poseVelocityPublisher.publish(poseVelocityMsg)
+
+def initialize_publisher_subscriber():
+    global poseVelocityPublisher, poseVelocitySubscriber
+    poseVelocityPublisher = rospy.Publisher('current_pose_velocity_publisher', poseVelocity)
+    poseVelocitySubscriber = rospy.Subscriber('/odom', Odometry, publishPoseAndVelocity)
 
 def main():
 # Initializes a rospy node so that the SimpleActionClient can
         # publish and subscribe over ROS.
     rospy.init_node('planning_client', log_level=rospy.DEBUG)
-    result = planning_client()
-    print("Result:", result)
+    planning_client()
+    initialize_publisher_subscriber()
+    rospy.spin()
 
 if __name__ == "__main__":
 
